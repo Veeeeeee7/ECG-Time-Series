29731d3672fbeced2c86c9cd143a8649f8683e06
Victor <victorli1076@gmail.com>
2025-01-02 22:56:52 -0800

    cleaned up splits and knn and minirocket

--- Baseline.py
+++ Baseline.py
@@ -3,11 +3,11 @@ import numpy as np
 import os
 from sklearn.metrics import accuracy_score, roc_auc_score, f1_score
 
-data = pd.read_csv('venv/data/MIMIC-IV-ECG-Ext-Electrolytes/baselines/carry_forward_24h-all_obtained/mimiciv_ECGv1.1_hospV2.2_Calcium50893.csv')
+data = pd.read_csv('/Volumes/SanDisk SSD/physionet.org/files/MIMIC-IV-ECG-Ext-Electrolytes/baselines/carry_forward_24h-all_obtained/mimiciv_ECGv1.1_hospV2.2_Calcium50893.csv')
 results = data['flag']
 predictions = data['carry_forward_lab_flag_last']
-results = results.replace({'abnormal': 1, np.nan: 0})
-predictions = predictions.replace({'abnormal': 1, np.nan: 0})
+results = results.map({'abnormal': 1}).fillna(0)
+predictions = predictions.map({'abnormal': 1}).fillna(0)
 accuracy = accuracy_score(results, predictions)
 
 auroc = roc_auc_score(results, predictions)
